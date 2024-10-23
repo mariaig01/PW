d1f691edabb4c683c3d3f475329147440b053bf3
María <101421468+mariaig01@users.noreply.github.com>
2024-10-23 20:46:33 +0200

    Update conexionbd.php

--- pe2/conexionbd.php
+++ pe2/conexionbd.php
@@ -1,8 +1,8 @@
 <?php 
      // Datos de conexión a la base de datos
      $dsn = "mysql:host=localhost;dbname=dbangustias16_pw2324";
-     $usuario = "pwangustias16";
-     $password = "23angustias1624";
+     $usuario = "";
+     $password = "4";
 
      $conexion = new PDO($dsn, $usuario, $password);
      $conexion->exec("SET NAMES utf8mb4"); 
